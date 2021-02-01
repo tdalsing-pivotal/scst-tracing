355b66edae1ebb3d89547832d9db5eea213b42ee
Tim Dalsing <tdalsing@vmware.com>
2021-02-01 11:20:33 -0500

    updates

--- producer/src/main/java/com/vmware/producer/ProducerApp.java
+++ producer/src/main/java/com/vmware/producer/ProducerApp.java
@@ -4,12 +4,7 @@ import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.sleuth.instrument.messaging.TracingChannelInterceptor;
-import org.springframework.cloud.stream.binding.BindingService;
 import org.springframework.cloud.stream.function.StreamBridge;
-import org.springframework.cloud.stream.messaging.DirectWithAttributesChannel;
-import org.springframework.context.annotation.Bean;
-import org.springframework.messaging.MessageChannel;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -27,26 +22,24 @@ import static org.springframework.http.HttpStatus.CREATED;
 public class ProducerApp {
 
     StreamBridge bridge;
-    BindingService bindingService;
 
-    public ProducerApp(StreamBridge bridge, BindingService bindingService) {
+    public ProducerApp(StreamBridge bridge) {
         this.bridge = bridge;
-        this.bindingService = bindingService;
     }
 
     public static void main(String[] args) {
         SpringApplication.run(ProducerApp.class, args);
     }
 
-    @Bean(name = "output-out-0")
-    public MessageChannel outputChannel(TracingChannelInterceptor interceptor) {
-        log.info("outputChannel");
-        DirectWithAttributesChannel channel = new DirectWithAttributesChannel();
-        channel.setAttribute("type", "output");
-        channel.addInterceptor(interceptor);
-        bindingService.bindProducer(channel, "output-out-0", false);
-        return channel;
-    }
+//    @Bean(name = "output-out-0")
+//    public MessageChannel outputChannel(TracingChannelInterceptor interceptor, BindingService bindingService) {
+//        log.info("outputChannel");
+//        DirectWithAttributesChannel channel = new DirectWithAttributesChannel();
+//        channel.setAttribute("type", "output");
+//        channel.addInterceptor(interceptor);
+//        bindingService.bindProducer(channel, "output-out-0", false);
+//        return channel;
+//    }
 
     @PostMapping("/")
     @ResponseStatus(CREATED)
